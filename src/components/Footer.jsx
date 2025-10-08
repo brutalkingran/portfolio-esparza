8df9b87d73c467c6f7f2863294bf8df2fc54a152
brutalkingran <swkvideogames@gmail.com>
2025-10-08 15:34:35 -0300

    main section format

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
-    <footer id="contact" className="bg-gray-900 text-white py-6 text-center">
-      <p>© Patricio Esparza, 2025 - 無限力</p>
+    <footer id="contact" className="bg-gray-900 text-white py-10 text-center skew-y-6">
+      <p className="-skew-y-6 mt-5">© Patricio Esparza, 2025 - 無限力</p>
     </footer>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,8 +2,29 @@ import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
-    <section id="projects" className="py-16 bg-white dark:bg-gray-900 relative text-white h-screen clip-diagonal skew-y-6 -mt-25">
-      
+    <section id="projects" className="py-16 bg-white dark:bg-gray-900 text-gray-900 relative h-screen clip-diagonal skew-y-6 -mt-25 font-light">
+      <div className="-skew-y-6 pt-20 px-10 flex flex-row justify-between">
+        <div className="w-1/2">
+          <h2 className="text-6xl">
+            Tu presencia en línea importa
+          </h2>
+
+          <p className="mt-15 font-sans text-2xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fugit dolore velit natus, atque repellendus neque minima qui. Aut fuga maiores repudiandae corporis sapiente accusantium velit molestiae harum dolore autem?
+            Sit cum vero, at omnis aliquid expedita. Rem expedita, modi blanditiis voluptas voluptatibus, ab totam sed et ea est assumenda quasi autem qui veniam animi deleniti eveniet quos pariatur doloribus!
+            Dolor similique voluptas error molestiae impedit sint porro. Distinctio, molestias reprehenderit nostrum et sequi doloremque tempore voluptatum sapiente at maxime incidunt natus quibusdam velit suscipit. Expedita placeat magnam in hic.
+            Fugiat quisquam, animi fuga et est velit nulla aspernatur blanditiis eius, quam fugit maxime incidunt temporibus magni obcaecati molestias aut earum. Eos nihil iusto explicabo facilis iste fugit maxime amet!
+          </p>
+        </div>
+
+        <figure className="flex flex-col items-center w-2/5 mt-15 mr-20">
+          <img src="cartx.jpg" alt="" className="skew-y-7 w-full"/>
+          
+          <figcaption className="text-center mt-15 font-medium w-2/3">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. A expedita provident molestiae nostrum dolorum itaque iure aliquid vero mollitia eligendi? Neque numquam eius vero necessitatibus at, impedit nisi molestias! Tempore!
+          </figcaption>
+        </figure>
+      </div>
     </section>
   );
 }
