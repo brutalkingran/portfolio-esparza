87d7db6a68b89ca2b26b38245d9ff68fa71b6f98
brutalkingran <swkvideogames@gmail.com>
2025-10-13 18:56:42 -0300

    secciones sin separar

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,6 @@
-import ProjectCard from "./ProjectCard";
-
 const Projects = () => {
   return (
+  <>
     <section id="projects" className="py-16 bg-white dark:bg-gray-900 text-gray-900 relative h-screen clip-diagonal skew-y-6 -mt-25 font-light">
       <div className="-skew-y-6 pt-20 px-10 flex flex-row justify-between">
         <div className="w-1/2">
@@ -16,16 +15,73 @@ const Projects = () => {
             Fugiat quisquam, animi fuga et est velit nulla aspernatur blanditiis eius, quam fugit maxime incidunt temporibus magni obcaecati molestias aut earum. Eos nihil iusto explicabo facilis iste fugit maxime amet!
           </p>
         </div>
-
+        
         <figure className="flex flex-col items-center w-2/5 mt-15 mr-20">
+          <h3 className="skew-y-7 text-3xl mb-2">Title</h3>
+
           <img src="cartx.jpg" alt="" className="skew-y-7 w-full"/>
           
-          <figcaption className="text-center mt-15 font-medium w-2/3">
+          <figcaption className="skew-y-7 text-center mt-5 font-medium w-2/3">
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. A expedita provident molestiae nostrum dolorum itaque iure aliquid vero mollitia eligendi? Neque numquam eius vero necessitatibus at, impedit nisi molestias! Tempore!
           </figcaption>
         </figure>
       </div>
     </section>
+
+    <section id="projects" className="py-16 bg-gray-400 dark:bg-gray-900 text-gray-100 relative h-screen clip-diagonal skew-y-6 -mt-25 font-light">
+      <div className="-skew-y-6 pt-20 px-10 flex flex-row-reverse justify-between">
+        <div className="w-1/2 mr-25">
+          <h2 className="text-6xl">
+            Tu presencia en línea importa
+          </h2>
+
+          <p className="mt-15 font-sans text-2xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fugit dolore velit natus, atque repellendus neque minima qui. Aut fuga maiores repudiandae corporis sapiente accusantium velit molestiae harum dolore autem?
+            Sit cum vero, at omnis aliquid expedita. Rem expedita, modi blanditiis voluptas voluptatibus, ab totam sed et ea est assumenda quasi autem qui veniam animi deleniti eveniet quos pariatur doloribus!
+            Dolor similique voluptas error molestiae impedit sint porro. Distinctio, molestias reprehenderit nostrum et sequi doloremque tempore voluptatum sapiente at maxime incidunt natus quibusdam velit suscipit. Expedita placeat magnam in hic.
+            Fugiat quisquam, animi fuga et est velit nulla aspernatur blanditiis eius, quam fugit maxime incidunt temporibus magni obcaecati molestias aut earum. Eos nihil iusto explicabo facilis iste fugit maxime amet!
+          </p>
+        </div>
+        
+        <figure className="flex flex-col items-center w-2/5 mt-15 -skew-y-7">
+          <h3 className="text-3xl mb-2">Title</h3>
+
+          <img src="cartx.jpg" alt="" className="w-full"/>
+          
+          <figcaption className="text-center mt-5 font-medium w-2/3">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. A expedita provident molestiae nostrum dolorum itaque iure aliquid vero mollitia eligendi? Neque numquam eius vero necessitatibus at, impedit nisi molestias! Tempore!
+          </figcaption>
+        </figure>
+      </div>
+    </section>
+
+    <section id="projects" className="py-16 bg-white dark:bg-gray-900 text-gray-900 relative h-screen clip-diagonal skew-y-6 -mt-25 font-light">
+      <div className="-skew-y-6 pt-20 px-10 flex flex-row justify-between">
+        <div className="w-1/2">
+          <h2 className="text-6xl">
+            Tu presencia en línea importa
+          </h2>
+
+          <p className="mt-15 font-sans text-2xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex fugit dolore velit natus, atque repellendus neque minima qui. Aut fuga maiores repudiandae corporis sapiente accusantium velit molestiae harum dolore autem?
+            Sit cum vero, at omnis aliquid expedita. Rem expedita, modi blanditiis voluptas voluptatibus, ab totam sed et ea est assumenda quasi autem qui veniam animi deleniti eveniet quos pariatur doloribus!
+            Dolor similique voluptas error molestiae impedit sint porro. Distinctio, molestias reprehenderit nostrum et sequi doloremque tempore voluptatum sapiente at maxime incidunt natus quibusdam velit suscipit. Expedita placeat magnam in hic.
+            Fugiat quisquam, animi fuga et est velit nulla aspernatur blanditiis eius, quam fugit maxime incidunt temporibus magni obcaecati molestias aut earum. Eos nihil iusto explicabo facilis iste fugit maxime amet!
+          </p>
+        </div>
+        
+        <figure className="flex flex-col items-center w-2/5 mt-15 mr-20">
+          <h3 className="skew-y-7 text-3xl mb-2">Title</h3>
+
+          <img src="cartx.jpg" alt="" className="skew-y-7 w-full"/>
+          
+          <figcaption className="skew-y-7 text-center mt-5 font-medium w-2/3">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. A expedita provident molestiae nostrum dolorum itaque iure aliquid vero mollitia eligendi? Neque numquam eius vero necessitatibus at, impedit nisi molestias! Tempore!
+          </figcaption>
+        </figure>
+      </div>
+    </section>
+  </>
   );
 }
 
