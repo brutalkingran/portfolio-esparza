c2ecb020ecac4b9ae81df8cfd4c52ed6a6d87e5d
brutalkingran <swkvideogames@gmail.com>
2025-10-15 17:11:14 -0300

    disclaimer moviles

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ import Navbar from "./components/Navbar";
 import Introduction from "./components/Introduction";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    if (window.innerWidth < 768) {
+      alert("Esta web sigue en construcción para móviles. Cambia tu vista a modo escritorio si es posible!")
+    }
+  }, [])
+
   return (
     <div className="font-sans">
       <Navbar />
