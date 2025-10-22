5bd5b78330cfeca99d274526ca13c4433f4e8607
brutalkingran <swkvideogames@gmail.com>
2025-10-22 14:39:12 -0300

    color update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer id="contact" className="bg-gray-900 dark:bg-gray-800 text-white dark:text-gray-100 md:py-10 mb-15 text-center">
       <p className="my-3 md:mt-5">© Patricio Esparza, 2025 - 無限力</p>
-      <p className="mt-1 md:mt-5">{ t("footer.sourceCode") }: <a className="text-blue-600 dark:text-blue-200 italic underline" href="https://github.com/brutalkingran/portfolio-esparza">portfolio-esparza</a></p>
+      <p className="mt-1 md:mt-5">{t("footer.sourceCode")}: <a className="text-blue-400 hover:text-white dark:text-blue-100 dark:hover:text-blue-50 italic underline" href="https://github.com/brutalkingran/portfolio-esparza">portfolio-esparza</a></p>
     </footer>
   );
 }
