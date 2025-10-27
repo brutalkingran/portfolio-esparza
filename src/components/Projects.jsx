c1b8595a277dfb8f60bc4dbf94c4ea224f03d900
brutalkingran <swkvideogames@gmail.com>
2025-10-27 15:00:41 -0300

    nuevo sitio añadido

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -120,6 +120,27 @@ const Projects = () => {
             imgSrc="work_scrsht"
             description={t("body.section3.figure1.desc")}
           />
+          <FigureComponent
+            title={t("body.section3.figure2.title")}
+            imgSrc="IDEargentina-index"
+            description={t("body.section3.figure2.desc")}
+            pageLink="https://swk.lyricaltokarev.com"
+            pageRepository="viajes-php"
+          />
+          <FigureComponent
+            title={t("body.section3.figure3.title")}
+            imgSrc="IDEargentina-administration"
+            description={t("body.section3.figure3.desc")}
+            pageLink="https://swk.lyricaltokarev.com"
+            pageRepository="viajes-php"
+          />
+          <FigureComponent
+            title={t("body.section3.figure4.title")}
+            imgSrc="IDEargentina-user-panel"
+            description={t("body.section3.figure4.desc")}
+            pageLink="https://swk.lyricaltokarev.com"
+            pageRepository="viajes-php"
+          />
         </FigureSweepComponent>
       </SectionComponent>
     </main >
